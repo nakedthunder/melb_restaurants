b655fcb8d0b7719f43f23ad28e89c487a99b5de4
Jess <nakedthunder00@gmail.com>
2024-04-14 08:11:34 +1000

    kakaoMap API controller

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
+/*
+**********************************************************
+1. 지도 생성 & 확대 축소 컨트롤러
+https://apis.map.kakao.com/web/sample/addMapControl/
+
+*/
+
 var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
 var options = {
   //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3, //지도의 레벨(확대, 축소 정도)
+  center: new kakao.maps.LatLng(37.54, 126.96), //지도의 중심좌표. 서울 한가운데
+  level: 8, //지도의 레벨(확대, 축소 정도) 3에서 8레벨로 확대
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+// 지도에 확대 축소 컨트롤을 생성
+let zoomControl = new kakao.maps.ZoomControl();
+
+// 지도의 우측에 확대 축소 컨트롤을 추가
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// 데이터 준비 : 서버에서 저장되는 데이터를 불러서 쓰는데, 지금은 local에서 더미데이터를 만든다.
+// 유튜브 영상 링크 => 내꺼?
+// 3개 데이터 넣기
+
+/*
+**********************************************************
+2. 더미데이터 준비하기 (제목, 주소, url, 카테고리)
+*/
+const dataSet = [
+  {
+    title: "희락돈까스",
+    address: "서울 영등포구 양산로 210",
+    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    category: "양식",
+  },
+  {
+    title: "즉석우동짜장",
+    address: "서울 영등포구 대방천로 260",
+    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    category: "한식",
+  },
+  {
+    title: "아카사카",
+    address: "서울 서초구 서초대로74길 23",
+    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    category: "일식",
+  },
+];
