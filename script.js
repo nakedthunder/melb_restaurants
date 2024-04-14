1f8e1acb6ac57f9be0c0c0cc2840ddf95fa5b1e9
Jess <nakedthunder00@gmail.com>
2024-04-15 05:47:20 +1000

    add the kakao Maps library to mark multiple locations on the map

--- script.js
+++ script.js
@@ -32,19 +32,64 @@ const dataSet = [
   {
     title: "희락돈까스",
     address: "서울 영등포구 양산로 210",
-    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    url: "https://www.youtube.com/watch?v=2ZBmpw23ZC4",
     category: "양식",
   },
   {
     title: "즉석우동짜장",
     address: "서울 영등포구 대방천로 260",
-    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    url: "https://www.youtube.com/watch?v=2ZBmpw23ZC4",
     category: "한식",
   },
   {
     title: "아카사카",
     address: "서울 서초구 서초대로74길 23",
-    url: "https://www.youtube.com/watch?v=1YOJbOUR4vw&t=88s",
+    url: "https://www.youtube.com/watch?v=2ZBmpw23ZC4",
     category: "일식",
   },
 ];
+
+/*
+**********************************************************
+3. 여러개 마커 찍기
+  * 주소 - 좌표 변환
+https://apis.map.kakao.com/web/sample/multipleMarkerImage/ (여러개 마커)
+https://apis.map.kakao.com/web/sample/addr2coord/ (주소로 장소 표시하기)
+*/
+// 주소 - 좌표 변환 함수 (비동기 문제 발생 해결) ****************
+// 주소-좌표 변환 객체를 생성합니다
+
+// Geocoder: 서비스 라이브러리를 불러와야한다. <script> 태그에서 src에 &libraries=services 추가함
+var geocoder = new kakao.maps.services.Geocoder();
+
+async function setMap() {
+  for (var i = 0; i < dataSet.length; i++) {
+    // getCoordsByAddress 주소를 좌표로 변환하는 코드를 반복문안에 넣어준다.
+    let position = await getCoordsByAddress(dataSet[i].address);
+    console.log(position);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      // position: positions[i].latlng, // 마커를 표시할 위치
+      position: position,
+      // title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+    });
+  }
+}
+function getCoordsByAddress(address) {
+  // promise 형태로 반환
+  return new Promise((resolve, reject) => {
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(address, function (result, status) {
+      // 정상적으로 검색이 완료됐으면
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        return resolve(coords);
+      }
+      reject(new Error("getCoordsByAddress Error: not valid Address"));
+    });
+  });
+}
+
+setMap();
